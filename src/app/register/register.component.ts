8b0ddc820d954e03f5847d8a3241fda40a7cab90
JeremyCM47 <jerry.c-m@live.fr>
2021-04-02 16:59:50 +0200

    validator password form (need fix)

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ErrorStateMatcher} from '@angular/material/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, Validators,FormGroup, FormBuilder} from '@angular/forms';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -9,28 +9,37 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 
-export class RegisterComponent implements OnInit {
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  passwordFormControl = new FormControl('', [
-    Validators.required
-  ]);
-  confirmPasswordFormControl = new FormControl('', [
-    Validators.required
-  ]);
+export class RegisterComponent {
+  
+  register: FormGroup;
+  
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.register = this.formBuilder.group({
+      emailFormControl: ['', [Validators.required, Validators.email]],
+      pseudoFormControl: ['', [Validators.required]],
+      passwordFormControl: ['', [Validators.required]],
+      confirmPasswordFormControl: ['']
+    }, { validator: this.checkPasswords });
 
-  matcher = new MyErrorStateMatcher();
-
-  ngOnInit(): void {
   }
+  
+  matcher = new MyErrorStateMatcher();
+  
+  
+    checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+      const password = group.get('password').value;
+      const confirmPassword = group.get('confirmPassword').value;
+    
+      return password === confirmPassword ? null : { notSame: true }     
+    }
 
 }
